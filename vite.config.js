7ed3109d9a7d2b1ca85aaaf912cf8f9a70a84306
Claudio Barca <c.barca@libero.it>
2023-09-14 17:28:13 +0200

    Bump packages - Replace vite-plugin-components (deprecated) with unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import {defineConfig} from "vite";
-import ViteComponents from "vite-plugin-components";
+import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
 import Pages from "vite-plugin-pages";
 import generateSitemap from "vite-plugin-pages-sitemap";
 import vue from "@vitejs/plugin-vue";
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     siteName: "vue-d3-dataviz-starter",
     resolve: {
-        alias: [{find: "@", replacement: "/src"}],
+        alias: [{ find: "@", replacement: "/src" }],
     },
     css: {
         preprocessorOptions: {
@@ -24,23 +24,23 @@ export default defineConfig({
     assetsInclude: ["**/*.woff", "**/*.woff2", "**/*.png"],
     filenameHashing: false,
     plugins: [
-        ViteComponents(),
+
         vue({
             include: [/\.vue$/, /\.md$/], // <--
         }),
-        ViteComponents({
+        Components({
             // allow auto load markdown components under `./src/components/`
-            extensions: ["vue", "md"],           
+            extensions: ["vue", "md"],
             // allow auto import and register components used in markdown
             customLoaderMatcher: id => id.endsWith(".md"),
         }),
         Pages({
             extensions: ["vue", "md"],
-            dirs: [{dir: "src/pages", baseRoute: ""}],
+            dirs: [{ dir: "src/pages", baseRoute: "" }],
             extendRoute(route, parent) {
                 console.log(route)
             },
-            
+
         }),
     ],
     ssgOptions: {
